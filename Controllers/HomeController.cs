8f7b987d29815337f8bf47fa92df99f4b1f3a49d
eduardocaas <eduardofc.dev@gmail.com>
2024-04-18 20:29:02 -0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JogadorAPI.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Get()
+            => Ok(new { status = "API is up to date", date = DateTime.Now.ToString("dd/MM/yyyy - HH:mm") });
+    }
+}
