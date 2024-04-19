690ad358312ffde54b9473d9c8eb7eb5d8e25f00
eduardocaas <eduardofc.dev@gmail.com>
2024-04-18 22:29:48 -0300

    Desabilitado Https e Swagger'

--- Program.cs
+++ Program.cs
@@ -3,20 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+/*if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+}*/
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
