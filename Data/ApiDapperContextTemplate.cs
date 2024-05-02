32dcb604eeaaf49cdc39f9ba9c33aecfbe94fa7c
eduardocaas <eduardofc.dev@gmail.com>
2024-05-02 19:07:39 -0300

    Add template for connection strings

---
+++ Data/ApiDapperContextTemplate.cs
@@ -0,0 +1,15 @@
+﻿namespace JogadorAPI.Data
+{
+    public static class ApiDapperContextModel
+    {
+        public const string CONNECTION_STRING_LOCAL = @"Server=YOURSERVER; 
+                                                        User ID=YOURUSERID; 
+                                                        Password=YOURPASSWORD; 
+                                                        Database=YOURDATABASE";
+
+        public const string CONNECTION_STRING_VM = @"Server=YOURSERVER; 
+                                                        User ID=YOURUSERID; 
+                                                        Password=YOURPASSWORD; 
+                                                        Database=YOURDATABASE";
+    }
+}
