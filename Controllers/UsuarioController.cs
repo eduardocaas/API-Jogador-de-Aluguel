6f5e524e9477fc1c97196022fd37833f42b9ef2c
eduardocaas <eduardofc.dev@gmail.com>
2024-05-02 18:40:11 -0300

    Add UsuarioController.cs

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JogadorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+    }
+}
