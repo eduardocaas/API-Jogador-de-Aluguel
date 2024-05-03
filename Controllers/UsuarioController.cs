d8b2260434238ffd86151b39420606ab3cf481e2
eduardocaas <eduardofc.dev@gmail.com>
2024-05-02 22:37:46 -0300

    Implemented Cadastro method

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JogadorAPI.Models;
+using JogadorAPI.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Net.Mime;
 
 namespace JogadorAPI.Controllers
 {
@@ -7,5 +11,18 @@ namespace JogadorAPI.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+
+        [HttpPost]
+        [Route("cadastro")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult Cadastro(
+            [FromBody] Usuario usuario,
+            [FromServices] MySqlConnection connection)
+        {
+            UsuarioRepository repository = new UsuarioRepository(connection);
+            var rows = repository.Create(usuario);
+            return Ok(rows);
+        }
     }
 }
