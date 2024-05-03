da6e0f5eeb4c1315539ada7d1ce3971f81cf7652
eduardocaas <eduardofc.dev@gmail.com>
2024-05-02 22:39:29 -0300

    Implemented native query for INSERT method

--- Repositories/UsuarioRepository.cs
+++ Repositories/UsuarioRepository.cs
@@ -1,12 +1,37 @@
-﻿using MySql.Data.MySqlClient;
+﻿using Dapper;
+using JogadorAPI.Models;
+using MySql.Data.MySqlClient;
+using Mysqlx.Crud;
 
 namespace JogadorAPI.Repositories
 {
-    public class UsuarioRepository : Repository<UsuarioRepository>
+    public class UsuarioRepository : Repository<Usuario>
     {
         private readonly MySqlConnection _connection;
 
         public UsuarioRepository(MySqlConnection connection) : base(connection)
             => _connection = connection;
+
+        public dynamic Create(Usuario usuario)
+        {
+            string sql = @"INSERT INTO Usuario 
+                               (EMAIL, TELEFONE, CPF, NOME, SENHA, CIDADE, BAIRRO) 
+                           VALUES 
+                               (@Email, @Telefone, @CPF, @Nome, @Senha, @Cidade, @Bairro)";
+
+            var data = new
+            {
+                Email = usuario.Email,
+                Telefone = usuario.Telefone,
+                CPF = usuario.CPF,
+                Nome = usuario.Nome,
+                Senha = usuario.Senha,
+                Cidade = usuario.Cidade,
+                Bairro = usuario.Bairro
+            };
+
+            var rowsAffected = _connection.Execute(sql, data);
+            return rowsAffected;
+        }
     }
 }
