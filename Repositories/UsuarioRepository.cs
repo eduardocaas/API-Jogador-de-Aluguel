95f1c119c482e86ee5edd5e31d13cf07209760b4
eduardocaas <eduardofc.dev@gmail.com>
2024-05-02 21:47:53 -0300

    Add UsuarioRepository.cs

---
+++ Repositories/UsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace JogadorAPI.Repositories
+{
+    public class UsuarioRepository : Repository<UsuarioRepository>
+    {
+        private readonly MySqlConnection _connection;
+
+        public UsuarioRepository(MySqlConnection connection) : base(connection)
+            => _connection = connection;
+    }
+}
