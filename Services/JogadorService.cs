fbaa880f7750c6c898bc69059793b2abbcd78c29
eduardocaas <eduardofc.dev@gmail.com>
2024-05-05 23:20:40 -0300

    Implemented Create methof

--- Services/JogadorService.cs
+++ Services/JogadorService.cs
@@ -1,6 +1,26 @@
-﻿namespace JogadorAPI.Services
+﻿using JogadorAPI.Models;
+using JogadorAPI.Repositories;
+using MySql.Data.MySqlClient;
+using Opw.HttpExceptions;
+using Scrypt;
+
+namespace JogadorAPI.Services
 {
-    public class JogadorService
+    public static class JogadorService
     {
+        public static void Create(
+            Jogador jogador,
+            MySqlConnection connection)
+        {
+            JogadorRepository repository = new JogadorRepository(connection);
+            ScryptEncoder encoder = new ScryptEncoder();
+
+            string hashSenha = encoder.Encode(jogador.Senha);
+            jogador.Senha = hashSenha;
+
+            var rows = repository.Create(jogador);
+            if (rows == 0)
+                throw new HttpException("Falha ao salvar dados no banco de dados, tente novamente mais tarde");
+        }
     }
 }
