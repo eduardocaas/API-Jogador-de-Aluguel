2e4a0e31d58c610ae0b344d470e15020fe5e796b
eduardocaas <eduardofc.dev@gmail.com>
2024-05-24 22:47:53 -0300

    Add CORS policy

--- Program.cs
+++ Program.cs
@@ -2,8 +2,14 @@ using JogadorAPI.Data;
 using JogadorAPI.Repositories;
 using MySql.Data.MySqlClient;
 
+var AllowAll = "_allowAll";
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(cors => cors.AddPolicy(name: AllowAll, policy =>
+{
+    policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+}));
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -27,6 +33,8 @@ var app = builder.Build();
 //app.UseHttpsRedirection();
 
 //app.UseAuthorization();
+
+app.UseCors(AllowAll);
 
 app.MapControllers();
 app.Run();
