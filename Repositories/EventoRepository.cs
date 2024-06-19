210a0d9bf24175ff1e1a15986d763f325198ba18
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 15:51:34 -0300

    Created native query

--- Repositories/EventoRepository.cs
+++ Repositories/EventoRepository.cs
@@ -10,7 +10,34 @@ namespace JogadorAPI.Repositories
         public EventoRepository(MySqlConnection connection) : base(connection)
             => _connection = connection;
         public dynamic Create(Evento evento)
-        { 
+        {
+            string sql1 = @"INSERT INTO Evento
+                            (USUARIO_ID, 
+                             CIDADE, 
+                             BAIRRO, 
+                             HORARIO, 
+                             DURACAO_MINUTOS,
+                             DESCRICAO,
+                             POSICAO) 
+                           VALUES
+                            (@UsuarioId, 
+                             @Cidade, 
+                             @Bairro, 
+                             @Horario, 
+                             @Duracao, 
+                             @Descricao,
+                             @Posicao)";
+
+            var data = new
+            {
+                UsuarioId = evento.IdUsuario,
+                Cidade = evento.Cidade,
+                Bairro = evento.Bairro,
+                Horario = evento.Horario,
+                Duracao = evento.DuracaoMinutos,
+                Descricao = evento.Descricao,
+                Posicao = evento.Posicao
+            };
         }
 
         public dynamic SelectEvento(int id)
