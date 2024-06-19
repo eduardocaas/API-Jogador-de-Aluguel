223c604d229564e9cf5c74be805b12f748ff194e
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 15:45:54 -0300

    Implemented fields

--- DBModels/EventoSelectDbModel.cs
+++ DBModels/EventoSelectDbModel.cs
@@ -2,5 +2,14 @@
 {
     public class EventoSelectDbModel
     {
+        public int ID_EVENTO { get; set; }
+        public string DESCRICAO { get; set; }
+        public string CIDADE { get; set; }
+        public string BAIRRO { get; set; }
+        public DateTime HORARIO { get; set; }
+        public ushort DURACAO_MINUTOS { get; set; }
+        public byte POSICAO { get; set; }
+        public ushort CUSTO { get; set; }
+        public string? NOME_JOGADOR { get; set; }
     }
 }
