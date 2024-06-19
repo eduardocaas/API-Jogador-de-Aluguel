3c6be64837f97d3ce016c42367185e850c3083ea
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 13:05:13 -0300

    Refactor fields

--- Models/Evento.cs
+++ Models/Evento.cs
@@ -11,28 +11,35 @@ namespace JogadorAPI.Models
 
         [Column("USUARIO_ID")]
         [Required(ErrorMessage = "ID de Usuário é obrigatório")]
-        public int IdUsuario { get; set; }
+        public int? IdUsuario { get; set; }
 
         [Column("JOGADOR_ID")]
         public int IdJogador { get; set; }
 
-        [Column("ENDERECO")]
-        [Required(ErrorMessage = "Endereço é obrigatório")]
-        [StringLength(100, ErrorMessage = "O endereço deve ter no máximo 100 caracteres")]
-        public string Endereco { get; set; }
+        [Column("CIDADE")]
+        [Required(ErrorMessage = "Cidade é obrigatória")]
+        [StringLength(80, ErrorMessage = "A cidade deve ter até 80 caracteres")]
+        public string Cidade { get; set; }
+
+        [Column("BAIRRO")]
+        [Required(ErrorMessage = "Bairro é obrigatório")]
+        [StringLength(80, ErrorMessage = "O bairro deve ter até 80 caracteres")]
+        public string Bairro { get; set; }
 
         [Column("HORARIO")]
         [Required(ErrorMessage = "Horário é obrigatório")]
-        public DateTime Horario { get; set; }
+        public DateTime? Horario { get; set; }
 
         [Column("DURACAO_MINUTOS")]
         [Required(ErrorMessage = "Duração é obrigatória")]
         [Range(30, 240, ErrorMessage = "A duração deve ser entre 30 e 240 minutos")]
-        public ushort DuracaoMinutos { get; set; }
+        public ushort? DuracaoMinutos { get; set; }
 
         [Column("DESCRICAO")]
-        [Required(ErrorMessage = "A descrição é obrigatória")]
         public string Descricao { get; set; }
+
+        [Column("STATUS")]
+        public byte Status { get; set; }
 
         [Column("CUSTO")]
         public ushort Custo { get; set; }
@@ -40,7 +47,7 @@ namespace JogadorAPI.Models
         [Column("POSICAO")]
         [Required(ErrorMessage = "A posição é obrigatória")]
         [Range(1, 3, ErrorMessage = "Escolha uma posição válida: 1 - Goleiro, 2 - Defesa, 3 - Ataque")]
-        public byte Posicao { get; set; }
+        public byte? Posicao { get; set; }
 
         [Column("DATA_CRIACAO")]
         public DateTime DataCriacao { get; set; }
