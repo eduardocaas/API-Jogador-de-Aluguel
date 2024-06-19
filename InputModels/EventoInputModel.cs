928a8981af320abc006852d6132dcfc20fe53bee
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 13:06:49 -0300

    Implemented fields

--- InputModels/EventoInputModel.cs
+++ InputModels/EventoInputModel.cs
@@ -1,6 +1,23 @@
 ﻿namespace JogadorAPI.InputModels
 {
-    public class EventoInputModel
+    public struct EventoInputModel
     {
+        [JsonPropertyName("Id_Usuario")]
+        [Required(ErrorMessage = "ID de Usuário é obrigatório")]
+        public int? IdUsuario { get; set; }
+        [Required(ErrorMessage = "Cidade é obrigatório")]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "Bairro é obrigatório")]
+        public string Bairro { get; set; }
+        [Required(ErrorMessage = "Horário é obrigatório")]
+        public DateTime? Horario { get; set; }
+        [JsonPropertyName("Duracao")]
+        [Required(ErrorMessage = "Duração é obrigatório")]
+        public ushort? DuracaoMinutos { get; set; }
+        [Required(ErrorMessage = "Posição é obrigatório")]
+        public byte? Posicao { get; set; }
+        [Required(ErrorMessage = "Descrição é obrigatório")]
+        public string Descricao { get; set; }
     }
 }
