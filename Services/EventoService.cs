61866ec7d92bef1135aa0c4a8147f019c71cb48b
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 15:55:48 -0300

    Implemented method Create

--- Services/EventoService.cs
+++ Services/EventoService.cs
@@ -1,5 +1,9 @@
-﻿using JogadorAPI.Models;
+﻿using JogadorAPI.DBModels;
+using JogadorAPI.Models;
+using JogadorAPI.Repositories;
 using JogadorAPI.ViewModels;
+using MySql.Data.MySqlClient;
+using Opw.HttpExceptions;
 
 namespace JogadorAPI.Services
 {
@@ -9,7 +13,23 @@ namespace JogadorAPI.Services
             Evento evento,
             MySqlConnection connection)
         {
+            EventoRepository repository = new EventoRepository(connection);
+            var id = repository.Create(evento);
+            if (id == 0 || id == null)
+                throw new HttpException("Falha ao salvar dados no banco de dados, tente novamente mais tarde");
 
+            EventoSelectDbModel eventoSelect = repository.SelectEvento(id);
+
+            return new EventoCreateViewModel(
+                eventoSelect.ID_EVENTO,
+                eventoSelect.DESCRICAO,
+                eventoSelect.CIDADE,
+                eventoSelect.BAIRRO,
+                eventoSelect.HORARIO,
+                eventoSelect.DURACAO_MINUTOS,
+                eventoSelect.POSICAO,
+                eventoSelect.CUSTO,
+                eventoSelect.NOME_JOGADOR);
         }
     }
 }
