8eb7f5240325321784a5d1cfd52eaf9f6fc8d6c1
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 15:48:51 -0300

    Configure dependency injection

--- Repositories/EventoRepository.cs
+++ Repositories/EventoRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace JogadorAPI.Repositories
+﻿using JogadorAPI.Models;
+using MySql.Data.MySqlClient;
+
+namespace JogadorAPI.Repositories
 {
-    public class EventoRepository
+    public class EventoRepository : Repository<Evento>
     {
+        private readonly MySqlConnection _connection;
+
+        public EventoRepository(MySqlConnection connection) : base(connection)
+            => _connection = connection;
     }
 }
