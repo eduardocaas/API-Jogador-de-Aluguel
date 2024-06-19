6411ed7b952f1579fecc12b5cb24249329278403
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 13:07:57 -0300

    Implemented fields

--- ViewModels/EventoCreateViewModel.cs
+++ ViewModels/EventoCreateViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace JogadorAPI.ViewModels
+﻿using System.Text.Json.Serialization;
+
+namespace JogadorAPI.ViewModels
 {
-    public class EventoCreateViewModel
+    public struct EventoCreateViewModel
     {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public DateTime Horario { get; set; }
+        [JsonPropertyName("Duracao")]
+        public ushort DuracaoMinutos { get; set; }
+        public byte Posicao { get; set; }
+        public ushort Custo { get; set; }
+        [JsonPropertyName("Nome_Jogador")]
+        public string? NomeJogador { get; set; }
     }
 }
