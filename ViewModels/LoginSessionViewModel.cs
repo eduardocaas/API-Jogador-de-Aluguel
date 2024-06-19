8eb77c5bd1b10042766daade1728b3f78447a926
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 16:04:21 -0300

    Implemented fields

--- ViewModels/LoginSessionViewModel.cs
+++ ViewModels/LoginSessionViewModel.cs
@@ -1,6 +1,12 @@
-﻿namespace JogadorAPI.ViewModels
+﻿using System.Text.Json.Serialization;
+
+namespace JogadorAPI.ViewModels
 {
-    public class LoginSessionViewModel
+    public record struct LoginSessionViewModel
     {
+        [JsonPropertyName("Id")]
+        public int Id { get; set; }
+        [JsonPropertyName("Email")]
+        public string Email { get; set; }
     }
 }
