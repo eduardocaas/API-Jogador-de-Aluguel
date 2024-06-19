428699ae49ce7488e2877b1b6acfcbe590a3e256
eduardocaas <eduardofc.dev@gmail.com>
2024-06-19 15:57:14 -0300

    Default config

---
+++ Controllers/EventoController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JogadorAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventoController : ControllerBase
+    {
+    }
+}
