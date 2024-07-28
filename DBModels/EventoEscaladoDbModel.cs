192ecd654e4c02a1653419a2457c0ff1f52fd4a0
eduardocaas <eduardofc.dev@gmail.com>
2024-07-27 23:53:51 -0300

    Implemented properties

--- DBModels/EventoEscaladoDbModel.cs
+++ DBModels/EventoEscaladoDbModel.cs
@@ -2,5 +2,27 @@
 {
     public class EventoEscaladoDbModel
     {
+        public EventoEscaladoDbModel(int? IdEvento, string? Descricao, string? Cidade, string? Bairro, DateTime? Horario, int? DuracaoMinutos, int? Posicao, int? Custo, string? NomeUsuario)
+        {
+            ID_EVENTO = IdEvento;
+            DESCRICAO = Descricao;
+            CIDADE = Cidade;
+            BAIRRO = Bairro;
+            HORARIO = Horario;
+            DURACAO_MINUTOS = DuracaoMinutos;
+            POSICAO = Posicao;
+            CUSTO = Custo;
+            NOME_USUARIO = NomeUsuario;
+        }
+
+        public int? ID_EVENTO { get; set; }
+        public string? DESCRICAO { get; set; }
+        public string? CIDADE { get; set; }
+        public string? BAIRRO { get; set; }
+        public DateTime? HORARIO { get; set; }
+        public int? DURACAO_MINUTOS { get; set; }
+        public int? POSICAO { get; set; }
+        public int? CUSTO { get; set; }
+        public string? NOME_USUARIO { get; set; }
     }
 }
